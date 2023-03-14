aaee1d0d5fe2efae220a32d037dadc8e7d7fb00c
jordannassar <jordan.nassar@tptlive.ee>
2023-03-14 20:08:40 +0200

    initial commit
    npm run dev

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import ToDo from "./components/ToDo.svelte";
+</script>
+
+<main>
+  <ToDo />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/ToDo.svelte
@@ -0,0 +1,78 @@
+<script>
+  let todos = [
+    { id: 1, text: "Learn Svelte" },
+    { id: 2, text: "Learn svelte again" },
+  ];
+  let newTodo = "";
+
+  function addTodo() {
+    if (newTodo !== "") {
+      todos = [
+        ...todos,
+        { id: Date.now(), text: newTodo },
+      ];
+      newTodo = "";
+    } else {
+      alert("Please enter a todo item");
+    }
+  }
+
+  function removeTodo(id) {
+    todos = todos.filter(
+      (todo) => todo.id !== id
+    );
+  }
+</script>
+
+<h1>2DO2DAY</h1>
+
+<input
+  bind:value={newTodo}
+  on:keydown={(event) =>
+    event.key === "Enter" && addTodo()}
+/>
+
+{#each todos as todo}
+  <div class="list">
+    <span style="margin-top: 12px; color: black">
+      {todo.text}
+    </span>
+    <button
+      class="removeButton"
+      on:click={() => removeTodo(todo.id)}
+      >DONE</button
+    >
+  </div>
+{/each}
+
+<style>
+  .removeButton {
+    color: GRAY;
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    padding-left: 24px;
+    padding-right: 24px;
+    border: none;
+    background: white;
+    margin-top: 6px;
+  }
+  .removeButton:hover {
+    color: green;
+  }
+  .list {
+    padding: 10px;
+    border: 0.5px solid gray;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    width: 20%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
